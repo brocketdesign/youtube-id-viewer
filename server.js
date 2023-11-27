de36c50f800afe99adde7c91af05e3efd9339742
brocketdesign <50971180+brocketdesign@users.noreply.github.com>
2023-11-27 09:25:07 +0900

    Update server.js

--- server.js
+++ server.js
@@ -11,10 +11,24 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.send('Hello MERN World!');
 });
-
 app.get('/video/:id', (req, res) => {
-    res.render('video', { videoId: req.params.id });
+    let videoId = req.params.id;
+
+    // Check if the ID is actually a full YouTube URL
+    if (videoId.includes('youtube.com')) {
+        // Extract the 'v' parameter from the URL
+        const urlParams = new URLSearchParams(new URL(videoId).search);
+        videoId = urlParams.get('v');
+    }
+    
+    // Check if videoId is valid before rendering
+    if (videoId) {
+        res.render('video', { videoId: videoId });
+    } else {
+        res.status(400).send('Invalid video ID or URL');
+    }
 });
+
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
